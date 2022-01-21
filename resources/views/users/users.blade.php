0f514c6afd9df28b4674fa81ab8fb20ee562a63c
yuusuke.oose <uiternia0910@gmail.com>
2022-01-21 02:58:08 +0000

    user pages

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,26 @@
+@if (count($users) > 0)
+    <ul class="list-unstyled">
+        @foreach ($users as $user)
+            <li class="media">
+                {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{ $user->name }}
+                    </div>
+                    <div>
+                        {{-- ユーザ詳細ページへのリンク --}}
+                        <ul class="dropdown-menu dropdown-menu-right">
+                                {{-- ユーザ詳細ページへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
+                                <li class="dropdown-divider"></li>
+                                {{-- ログアウトへのリンク --}}
+                                <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                            </ul>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    {{ $users->links() }}
+@endif
