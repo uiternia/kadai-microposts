46eb0831be6f474ff6f4c96886bdaf9b5dc36080
yuusuke.oose <uiternia0910@gmail.com>
2022-01-24 07:24:22 +0000

    follow, unfollow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,12 @@
                         <h3 class="card-title">{{ Auth::user()->name }}</h3>
                     </div>
                     <div class="card-body">
-                        {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                        {{-- 認証済みユーザのメールアドレスをもとにGravatarを取得して表示 --}}
                         <img class="rounded img-fluid" src="{{ Gravatar::get(Auth::user()->email, ['size' => 500]) }}" alt="">
                     </div>
                 </div>
             </aside>
             <div class="col-sm-8">
-                {{-- 投稿フォーム --}}
-                @include('microposts.form')
                 {{-- 投稿一覧 --}}
                 @include('microposts.microposts')
             </div>
